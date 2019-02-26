0683c3b9696fe7f7205fa89e8f047578340a0e78
Ramon Ferreira <rferreira_22@yahoo.com.br>
2019-02-26 19:30:08 -0300

    Arquivos HTML adicionados as rotas - brincando com HTML

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const app = express();
 
+// Criação de rotas
+// ***************************************
 app.get("/", function(req, res) {
-    res.send("Seja bem-vindo ao meu app!");
+    res.sendFile(__dirname+"/html/index.html")
 });
 
 app.get("/sobre", function(req, res) {
-    res.send("Minha pagina sobre!");
+    res.sendFile(__dirname+"/html/sobre.html");
 });
 
 app.get("/blog", function(req, res) {
-    res.send("Bem-vindo ao meu blog!");
+    res.sendFile(__dirname+"/html/blog.html");
 });
 
 app.get("/ola/:cargo/:nome", function(req, res) {
     res.send("<h1>Ola " + req.params['nome'] + "</h1>" +
             "<h2> Seu cargo e: " + req.params['cargo'] + "</h2>");
 })
+// ***************************************
 
-app.listen(8081, function() {
-    console.log("Servidor rodando em http://localhost:8081/");
+// Leitura da aplicação na porta e função de callback para monitoramento
+app.listen(8080, function() {
+    console.log("Servidor rodando em http://localhost:8080/");
 });
-//Localhost: 8081
+//Localhost: 8080
